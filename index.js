95aa8edae6c91d8d6c981bd422980ef1d86c1c79
Andrés Rodríguez Váquez <andres.rod.vaz.77@gmail.com>
2026-03-17 10:58:35 +0200

    updateui

--- index.js
+++ index.js
@@ -30,13 +30,10 @@ morgan.token("body", (req) => JSON.stringify(req.body));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
-
-app.get("/", (request, response) => {
-	response.send("<h1>Wellcome to the phonebook</h1>");
-});
 
 app.get("/info", (request, response) => {
 	const nPeople = people.length;
